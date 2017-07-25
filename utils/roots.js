c644cb10e7b27d66997dbc0d03cff94c297c0db7
Ashwin Saxena <asax27@sapient.com>
2017-07-25 19:06:21 +0530

    BookSource change

--- utils/roots.js
+++ utils/roots.js
@@ -1,5 +1,5 @@
 export function getBooks() {
-  return fetch('https://api.spencerdixon.com/users/1/books', {
+  return fetch('../books.json', {
     headers: {
       Accept: 'application/vnd.life-api.com; version=1',
       'Content-Type': 'application/json',
